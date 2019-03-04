bf594707fbf4fe72152b89547ef76b36ac4deaf7
Danylowow <48218211+Danylowow@users.noreply.github.com>
2019-03-04 19:10:37 +0200

    ogo python

---
+++ pythonogo.py
@@ -0,0 +1,6 @@
+print("hola")
+putania1=input("vusokosny rik: ")
+if putania1=="2020" or putania1=="2016" or putania1=="2012"  or putania1=="2008" or putania1=="2004" or putania1=="2000":
+	print("yes")
+else:
+	print("no")
